5d2fa5da10bacbcb69f05c58ef84c221873e2b3d
Saronny <101633650+Saronny@users.noreply.github.com>
2023-03-24 14:00:30 +0100

    Added login screen

--- login.py
+++ login.py
@@ -7,16 +7,19 @@ class Login:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.user = None
         
         
     def Login(self):
         if self.username == "admin" and self.password == "admin123":
-            return persons.Admin()
+            self.user = persons.Admin()	
+            return self.user
         else:
-            with open( "Analysis-3-herkansing\Data\Members.csv", "r") as file:
+            with open( "Data\Members.csv", "r") as file:
                 reader = csv.reader(file, delimiter=";")
                 for row in reader:
                     if self.username == row[7] and self.password == row[8]:
                         self.user = persons.User(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[9])
+                        return self.user
                     else:
                         self.user = None
