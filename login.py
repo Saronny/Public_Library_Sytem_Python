d8d7a25c6a0cc020d255be317be2b569f6ac3f7d
Saronny <101633650+Saronny@users.noreply.github.com>
2023-04-09 01:11:47 +0200

    ...

--- login.py
+++ login.py
@@ -1,25 +1,26 @@
 import csv
 import persons
 import os
-class Login:
 
+
+class Login:
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.user = None
-        
-        
+
     def Login(self):
         if self.username == "admin" and self.password == "admin123":
-            self.user = persons.Admin()	
+            self.user = persons.Admin()
             return self.user
         else:
             with open('Data\Members.csv', 'r') as csvfile:
-                reader = csv.reader(csvfile, delimiter=';' , quotechar="'")
+                reader = csv.reader(csvfile, delimiter=';', quotechar="'")
                 for row in reader:
                     if self.username == row[7] and self.password == row[8]:
-                        self.user = persons.User(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[9])
+                        self.user = persons.User(
+                            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[9])
                         return self.user
                     else:
                         self.user = None
